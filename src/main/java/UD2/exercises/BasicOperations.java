1713b1cc3f725009c3efe16c6acea52175677017
Ilygomzzz <javier.sego04@gmail.com>
2024-11-17 20:05:47 +0100

    Operació multiplicacio

--- src/main/java/UD2/exercises/BasicOperations.java
+++ src/main/java/UD2/exercises/BasicOperations.java
@@ -9,10 +9,13 @@ public class BasicOperations {
         int a = in.nextInt();
         int b = in.nextInt();
 
+
         int suma = a + b;
         int resta = a - b;
+        int multiplicacio = a * b;
 
         System.out.println("Suma: " + suma);
         System.out.println("Resta: " + resta);
+        System.out.println("Multiplicació: " + multiplicacio);
     }
 }
