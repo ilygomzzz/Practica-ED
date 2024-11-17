940402be43a8a037dc5723287ed85e313061bf68
Ilygomzzz <javier.sego04@gmail.com>
2024-11-17 20:06:47 +0100

    Operació divisio

--- src/main/java/UD2/exercises/BasicOperations.java
+++ src/main/java/UD2/exercises/BasicOperations.java
@@ -8,5 +8,9 @@ public class BasicOperations {
         Scanner in = new Scanner(System.in).useLocale(Locale.US);
         int a = in.nextInt();
         int b = in.nextInt();
+
+        int divisio = a / b;
+
+        System.out.println("Divisió: " + divisio);
     }
 }
