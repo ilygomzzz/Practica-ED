a4c989c51d913209728c7dd41b7a36f5e6836a1e
Ilygomzzz <javier.sego04@gmail.com>
2024-11-17 20:01:39 +0100

    Added BasicOperations

---
+++ src/main/java/UD2/exercises/BasicOperations.java
@@ -0,0 +1,12 @@
+package UD2.exercises;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class BasicOperations {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+        int a = in.nextInt();
+        int b = in.nextInt();
+    }
+}
