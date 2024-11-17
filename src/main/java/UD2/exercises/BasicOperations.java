a3b50b1a63e4a99507c38c76065ce3490b70b80c
Ilygomzzz <javier.sego04@gmail.com>
2024-11-17 20:02:49 +0100

    Operació suma

--- src/main/java/UD2/exercises/BasicOperations.java
+++ src/main/java/UD2/exercises/BasicOperations.java
@@ -8,5 +8,9 @@ public class BasicOperations {
         Scanner in = new Scanner(System.in).useLocale(Locale.US);
         int a = in.nextInt();
         int b = in.nextInt();
+
+        int suma = a + b;
+
+        System.out.println("Suma: " + suma);
     }
 }
